4cf02d77eddebbe958b3d9457ca595cfa74f5a2b
Wei Huang <huangwei19810803@gmail.com>
2023-05-31 13:02:59 -0600

    add sql injection

--- bootcamp_app/students.js
+++ bootcamp_app/students.js
@@ -9,7 +9,7 @@ const pool = new Pool({
 
   const args = process.argv.slice(2);
 
-  pool.query(`select s.id, s.name as student, c.name as cohort from students s inner join cohorts c on s.cohort_id = c.id where c.name LIKE '${args[0]}%' limit ${args[1]};`)
+  pool.query(`select s.id, s.name as student, c.name as cohort from students s inner join cohorts c on s.cohort_id = c.id where c.name LIKE $1 limit $2;`, [`%${args[0]}%`, `${args[1]}`])
   .then(res => {
   res.rows.forEach(user => {
     console.log(`${user.student} has an id of ${user.id} and was in the ${user.cohort} cohort`);
--- bootcamp_app/teachers.js
+++ bootcamp_app/teachers.js
@@ -9,7 +9,7 @@ const pool = new Pool({
 
   const args = process.argv.slice(2);
 
-  pool.query(`select c.name as cohort, t.name as teacher from assistance_requests a inner join teachers t on a.teacher_id = t.id inner join students s on s.id = a.student_id inner join cohorts c on c.id = s.cohort_id where c.name LIKE '%${args[0]}%' group by c.id, t.id;`)
+  pool.query(`select c.name as cohort, t.name as teacher from assistance_requests a inner join teachers t on a.teacher_id = t.id inner join students s on s.id = a.student_id inner join cohorts c on c.id = s.cohort_id where c.name LIKE $1 group by c.id, t.id;`, [`%${args[0]}%`])
   .then(res=>{
     res.rows.forEach(ele=>{
         console.log(`${ele.cohort}: ${ele.teacher}`);
